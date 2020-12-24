10f1a80098746ce19eebbf12eeaca65ce0e32036
Jackson Hsieh <jackson97042@gmail.com>
2020-12-24 19:34:17 +0800

    feat: add index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const Record = require('../../models/Record')
+const Category = require('../../models/Category')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('works')
+const fetchAllData = async (model) => {
+  try {
+    //catch can detect err on Promise chain (tested)
+    const allData = await model.find().lean().then(allData => allData) 
+    return allData
+  } catch(err) {
+    console.log(err)
+  } 
+}
+
+router.get('/', async (req, res) => {
+  const categories = await fetchAllData(Category)
+  const icons = {}
+  for (category of categories) {
+    icons[category.name] = { name: category.icon }
+  }
+  const records = await fetchAllData(Record)
+  res.render('index', { records, icons, categories })
 })
+
 
 module.exports = router
