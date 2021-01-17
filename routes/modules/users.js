cbe4c6f46bc5a3059a7f32f1cbd38b6f4e3f3bf5
Jackson Hsieh <jackson97042@gmail.com>
2021-01-17 14:48:00 +0800

    feat: add users router for login/register feature

---
+++ routes/modules/users.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const Record = require('../../models/Record')
+const Category = require('../../models/Category')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.send('login page')
+})
+
+router.post('/login', (req, res) => {
+  res.send('Post login')
+})
+
+router.get('/register', (req, res) =>　{
+  res.send('register page')
+})
+
+router.post('/register', (req, res) => {
+  res.send('post register')
+})
+
+router.post('/logout', (req, res) => {
+  res.send('logout')
+})
+
+module.exports = router
